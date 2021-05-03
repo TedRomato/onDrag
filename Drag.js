4024973a7e2cda7a60a11714deb2eaee816342c6
TedRomato <59472129+TedRomato@users.noreply.github.com>
2021-05-03 13:13:03 +0200

    Update Drag.js

--- Drag.js
+++ Drag.js
@@ -1,78 +1,91 @@
-
-export default class Drag{
-  constructor(element){
+export default class CursorToBorderProximity{
+  constructor(element, checkInterval_ms = 100){
     this.element = element;
-
     this.mousePosition = [0,0];
-    this.mouseDown = false;
-    this.positionDifference = [0,0];
-
+    this.checkInterval = checkInterval_ms;
+    this.mousePressed = false;
+    this.mouseStateChanged = false;
     this.callback = undefined;
+    this.dragMousePositionStart = undefined;
+    this.cursorInElement = undefined;
+    this.running = true;
 
-    this.handleCalllback = () => {
-      this.callback(this.positionDifference);
+    this.handleElementMouseMove = ( e ) => {
+      this.cursorInElement = true;
+      const positionX = this.element.position().left;
+      const positionY = this.element.position().top;
+      this.mousePosition = [e.pageX - positionX, e.pageY - positionY]
     }
 
     this.handleWindowMouseMove = ( ) => {
       if(!this.cursorInElement){
-        this.mousePosition = [null, null]
-        this.mouseDown = false;
-
+        this.mousePosition = [null, null];
+        this.mousePressed = false;
       }
       this.cursorInElement = false;
     }
 
-    this.handleMouseMove = ( e, callback ) => {
-      this.cursorInElement = true;
-
-      if(this.mouseDown){
-
-        let xDifference = this.mousePosition[0] - event.pageX;
-        let yDifference = this.mousePosition[1] - event.pageY;
-        this.positionDifference = [xDifference,yDifference];
-        this.handleCalllback();
-
-      }
-
-      const positionX = this.element.position().left;
-      const positionY = this.element.position().top;
-      this.mousePosition = [e.pageX - positionX, e.pageY - positionY]
-
-    }
-
     this.handleMouseDown = ( ) => {
-      console.log("Pressed");
-      this.mouseDown = true;
+      this.mousePressed = true;
+      this.mouseStateChanged = true;
     }
 
     this.handleMouseUp = ( ) => {
-      this.mouseDown = false;
-      console.log("Released");
+      this.mouseStateChanged = true;
+      this.mousePressed = false;
     }
 
   }
 
-  onDrag( callback ){
-
+  onDrag(callback){
     this.callback = callback;
+    this.running = true;
+
+    //If mouse move didnt happen on element this global mouse move
+    //cursor is outside the element
+    //so we set mouse position to null, not to trigger border proximity
 
     $( window ).on('mousemove',this.handleWindowMouseMove);
-
-    $( this.element ).on('mousemove', this.handleMouseMove);
-
+    $( this.element ).on('mousemove', this.handleElementMouseMove);
     $( this.element ).on('mousedown', this.handleMouseDown);
+    $( this.element ).on('mouseup', this.handleMouseUp);
 
-    $( this.element ).on('mouseup',  this.handleMouseUp);
+    this.listen();
+  }
+
+
+  offDrag(){
+    $( this.element ).off('mousemove', this.handleWindowMouseMove);
+    $( this.element ).off('mousemove', this.handleElementMouseMove);
+
+    this.running = false;
+  }
+
+
+  async listen(){
+    this.mouseStateChanged = false;
+    this.dragMousePositionStart = [this.mousePosition[0], this.mousePosition[1]];
+    console.log(this.mousePressed);
+    setTimeout(() => {
+      this.callback(this.getCallbackArgument());
+      if(this.running){
+        this.listen();
+      }
+    },this.checkInterval);
 
   }
 
-  offDrag(){
+  getCallbackArgument(){
+    if(this.mouseStateChanged) return null
+    if(!this.mousePressed) return null
+    return this.getMousePositionDifference();
+  }
 
-    $( this.element ).off('mousemove', this.handleMouseMove);
+  getMousePositionDifference(){
+    if (!this.mousePosition[0] || !this.mousePosition[1]) return null;
 
-    $( this.element ).off('mousedown', this.handleMouseDown);
-
-    $( this.element ).off('mouseup', this.handleMouseUp);
+    return [this.mousePosition[0] - this.dragMousePositionStart[0], this.mousePosition[1] - this.dragMousePositionStart[1]];
 
   }
 }
+
